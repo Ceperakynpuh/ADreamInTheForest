edd5f0cb3c0b58883917a846106a7f6f973fcc87
Ceperakynpuh <100066431+Ceperakynpuh@users.noreply.github.com>
2022-03-05 22:05:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ADreamInTheForest
+
+Developed with Unreal Engine 4
---
+++ Source/ADreamInTheForest/ADreamInTheForestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ADreamInTheForestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADREAMINTHEFOREST_API AADreamInTheForestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
